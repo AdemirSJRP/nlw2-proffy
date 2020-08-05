716814c0ba1911651bb688e9a5a364c4aa4a1848
Ademir Rodrigues da Silva <ademiritumbiara@outlook.com>
2020-08-05 19:26:06 -0300

    commit inicial

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import whatsAppIcon from '../../assets/icons/whatsapp.svg';
+import api from '../../services/api';
+
+import './styles.css';
+
+export interface Teacher {
+  id: number;
+  name: string;
+  avatar: string;
+  whatsapp: string;
+  bio: string;
+  cost: number;
+  subject: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  const { id, name, avatar, whatsapp, bio, cost, subject } = teacher;
+
+  const createNewConnection = async () => {
+    try {
+      await api.post('connections', { user_id: id });
+      alert('conexão efetuada com sucesso');
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  return (
+    <article className="teacher-item">
+      <header>
+        <img src={avatar} alt={name} />
+        <div>
+          <strong>{name}</strong>
+          <span>{subject}</span>
+        </div>
+      </header>
+      <p>{bio}</p>
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$ {cost.toFixed(2)}</strong>
+        </p>
+        <a
+          href={`https://wa.me/${whatsapp}?text=Você recebeu uma mensagem de Proffy`}
+          target="_blank"
+          onClick={createNewConnection}
+        >
+          <img src={whatsAppIcon} alt="WhatsApp" />
+          Entrar em Contato
+        </a>
+      </footer>
+    </article>
+  );
+};
+
+export default TeacherItem;
